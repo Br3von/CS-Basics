f2dfa87e9a68a85166af8f542c31a8b09f9b0787
Br3von <72634450+Br3von@users.noreply.github.com>
2021-06-26 23:42:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C__Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           Random numberGen = new Random();
+
+
+            int roll1 = 0;
+            int roll2 = 1;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice");
+
+        
+            while(roll1 != roll2)
+            {
+                Console.ReadKey();
+
+                roll1 = numberGen.Next(1, 11);
+                roll2 = numberGen.Next(1, 11);
+                
+                Console.WriteLine("You rolled a " + roll1 + " and a " + roll2);
+
+                attempts++;
+            }
+
+                Console.WriteLine("It took you " + attempts + " attempts to roll a matching pair.");
+
+            //Wait before closing
+            Console.ReadKey();
+        }
+    }
+}
